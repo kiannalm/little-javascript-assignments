69a93af39891ab2b90c3cea08ea808c6d2b1fac2
Kianna L Moore <kianna.lm@gmail.com>
2016-11-15 15:44:10 -0600

    Loop Labs

---
+++ labs/loops/lab-02.js
@@ -0,0 +1,20 @@
+var nums = [1, 6, 42, 3, 9];
+
+var i = 0;
+
+while(i < nums.length) {
+  var number = nums[i];
+  console.log(number);
+  i++;
+}
+
+
+/*
+Write a while loop and console.log the number out of the array. So there should be five logs each, each with those values. So...
+Expected output:
+1
+6
+42
+3
+9
+*/
