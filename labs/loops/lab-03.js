9545b63a3ee392728f12655c777751e1a843a41d
Kianna L Moore <kianna.lm@gmail.com>
2016-11-15 16:31:49 -0600

    Lab 3

---
+++ labs/loops/lab-03.js
@@ -0,0 +1,19 @@
+var nums = [1, 6, 42, 3, 9];
+var sum = 0;
+
+for(var nums i = 0; i < nums.lenghth; i++) {
+  sum += nums [i] + ', ';
+  sum += '.'
+
+  para.textContent = sum;
+}
+
+//loop goes here
+
+console.log(sum);
+
+/*
+Write a for loop and compute the sum of the values, then console log it.
+Expected output:
+61
+*/
