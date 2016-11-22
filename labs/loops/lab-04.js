05d5632dce6664c5721aa6aec0c7c60e19d0a0e4
Kianna L Moore <kianna.lm@gmail.com>
2016-11-22 09:51:13 -0600

    Lab 4

---
+++ labs/loops/lab-04.js
@@ -0,0 +1,36 @@
+
+var nums = [1, 6, 42, 3, 9];
+var sum = 0;
+
+var i = 0
+i++;
+
+for  {
+
+  varResults = nums[i];
+}
+
+//loop goes here
+
+console.log(sum);
+
+/*
+Write a while loop and compute the sum of the values, then console log it.
+Expected output:
+61
+*/
+
+
+
+/*
+var i = 0;
+
+while(i < cats.length) {
+  if(i === cats.length - 1) {
+    info += 'and ' + cats[i] + '.';
+  } else {
+    info += cats[i] + ', ';
+  }
+
+  i++;
+}*/
